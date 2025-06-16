0c9b47a8a6e44c73cad81cfb76c393363e973835
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-06-16 18:06:29 +0300

    folder ok 2

--- filesharing/serializers.py
+++ filesharing/serializers.py
@@ -32,15 +32,24 @@ class FolderSerializer(serializers.ModelSerializer):
         model = Folder
         fields = ['id', 'name', 'collection', 'parent', 'created_at']
 
+    def to_internal_value(self, data):
+        logger.debug(f"FolderSerializer to_internal_value input: {data}")
+        data = data.copy()  # Створюємо копію, щоб не змінювати оригінал
+        if 'parent' not in data:
+            data['parent'] = None
+            logger.debug("Set parent to None")
+        validated_data = super().to_internal_value(data)
+        logger.debug(f"FolderSerializer to_internal_value output: {validated_data}")
+        return validated_data
+
     def validate(self, data):
+        logger.debug(f"FolderSerializer validate input: {data}")
         request = self.context.get('request')
         if request and request.user:
             collection = data.get('collection')
             if collection.project and collection.project.company != request.user.company:
                 logger.error(f"User {request.user.email} attempted to access collection {collection.id} from another company")
                 raise serializers.ValidationError("You do not have access to this collection.")
-        if 'parent' not in data:
-            data['parent'] = None
         return data
 
 class PhotoSerializer(serializers.ModelSerializer):
